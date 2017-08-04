48558ad44c4a5ab1b24c4d65d10591c7530f9b91
Steve Purcell <steve@sanityinc.com>
2017-08-05 09:10:12 +1200

    Add dir locals to force hindent in Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (hindent-reformat-buffer-on-save . t)
+  (haskell-stylish-on-save . nil)))
