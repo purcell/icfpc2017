b2fe7ffba94dd43d8c1a35e1426589b097f693fd
Steve Purcell <steve@sanityinc.com>
2017-08-05 09:57:41 +1200

    Basic types, close to the JSON format

---
+++ src/Types.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Types where
+
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Set (Set)
+import GHC.Generics (Generic)
+
+type SiteID = Int
+
+type PunterID = Int
+
+data Site = Site
+  { id :: SiteID
+  , x :: Double
+  , y :: Double
+  } deriving (Eq, Ord, Show, Generic)
+
+data River = River
+  { source :: SiteID
+  , target :: SiteID
+  } deriving (Eq, Ord, Show, Generic) -- TODO: custom Eq, Ord
+
+data Map = Map
+  { sites :: Set Site
+  , rivers :: Set River
+  , mines :: Set SiteID
+  } deriving (Eq, Ord, Show, Generic)
+
+data Move
+  = Claim { punter :: PunterID
+          , river :: River }
+  | Pass { punter :: PunterID }
+  deriving (Eq, Show)
