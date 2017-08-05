19ba44429cafa0665fb1afa750e8c85d2fd47cdd
Steve Purcell <steve@sanityinc.com>
2017-08-05 17:25:10 +1200

    Handle length prefixes, and flush stdout when writing

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,27 @@
 
 module Main where
 
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as BSL
 import Data.Semigroup ((<>))
 import Offline
-import System.IO (stderr)
+import System.IO (hFlush, hPutStrLn, stderr, stdin, stdout)
 
 main :: IO ()
 main = play "flux-ambassadors" reader writer
   where
     reader = do
-      l <- B.getLine
-      B.hPutStrLn stderr ("Received: " <> l)
-      return (BSL.fromStrict l)
+      firstChunk <- B.hGet stdin 10
+      let (initial, rest) = B.break (== ':') firstChunk
+          len = read (B.unpack initial)
+          afterColon = B.tail rest
+      do more <- B.hGet stdin (len - B.length afterColon)
+         let msg = afterColon <> more
+         B.hPutStrLn stderr ("Received: " <> firstChunk <> more)
+         return (BSL.fromStrict msg)
     writer :: BSL.ByteString -> IO ()
     writer s =
-      let s' = (BSL.toStrict s)
-      in do B.hPutStrLn stderr ("Sending:  " <> s')
-            B.putStr s'
+      let msg = show (BSL.length s) <> ":" <> BSL.unpack s
+      in do hPutStrLn stderr ("Sending:  " <> msg)
+            putStr msg
+            hFlush stdout
