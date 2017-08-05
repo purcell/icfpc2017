30d6a9236c9019618aec22a47c14ac5d48c39e8a
Steve Purcell <steve@sanityinc.com>
2017-08-05 18:49:05 +1200

    Apply a less stupid strategy

--- src/GamePlay.hs
+++ src/GamePlay.hs
@@ -4,8 +4,12 @@
 module GamePlay where
 
 import Data.Aeson
+import Data.Foldable (concatMap)
+import Data.List (sortBy)
+import Data.Maybe (listToMaybe)
+import Data.Ord (comparing)
 import Data.Semigroup ((<>))
-import Data.Set (Set)
+import Data.Set (Set, (\\))
 import qualified Data.Set as S
 import GHC.Generics (Generic)
 import Types
@@ -37,8 +41,39 @@ precomputeGameState s = GameState s []
 updateState :: [Move] -> GameState -> GameState
 updateState moves s = s {prevMoves = (prevMoves s <> moves)}
 
-nextMove :: GameState -> (Move, GameState)
-nextMove s = (Pass (myPunterID s), s)
-
 myPunterID :: GameState -> PunterID
 myPunterID s = GamePlay.punter (initialState s)
+
+nextMove :: GameState -> (Move, GameState)
+nextMove s = (ClaimMove bestClaim, s)
+  where
+    bestClaim = Claim (myPunterID s) (bestUnclaimedRiver s)
+
+connectedTo :: River -> SiteID -> Bool
+connectedTo (River s t) site = s == site || t == site
+
+bestUnclaimedRiver :: GameState -> River
+bestUnclaimedRiver s =
+  case listToMaybe $ sortBy (flip $ comparing riverScore) unclaimedRivers of
+    Just r -> r
+    _ -> error "how could there not be an unclaimed river?"
+  where
+    theMap = GamePlay.map (initialState s)
+    unclaimedRivers = S.toList $ allRivers \\ claimedRivers
+    allRivers = rivers theMap
+    claimedRivers = S.map river (claims s)
+    mySites :: Set SiteID
+    mySites =
+      S.fromList $
+      concatMap
+        (\c ->
+           if (myPunterID s == Types.punter c)
+             then [source (river c), target (river c)]
+             else [])
+        (S.toList (claims s))
+    mineSites :: Set SiteID
+    mineSites = mines theMap
+    riverScore :: River -> Int
+    riverScore r =
+      (1 + 30 * S.size (S.filter (connectedTo r) mineSites)) *
+      (1 + 10 * S.size (S.filter (connectedTo r) mySites))
