ceb03847eb358b2635fa30e3a4d68fb0b4df9379
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2017-08-05 14:22:23 +1200

    Visualise claimed rivers for first 4 punters as different colours & widths

--- src/Visualiser.hs
+++ src/Visualiser.hs
@@ -2,6 +2,7 @@
 
 module Visualiser where
 
+import Data.Maybe (listToMaybe)
 import Data.Monoid ((<>))
 import qualified Data.Set as Set
 import Text.Blaze.Svg.Renderer.String (renderSvg)
@@ -10,19 +11,19 @@ import qualified Text.Blaze.Svg11 as S
 import qualified Text.Blaze.Svg11.Attributes as A
 import Types
 
-render :: IO ()
-render = do
-  let a = renderSvg $ mapToSvg sampleMap
-  writeFile "visualiser-output.svg" a
+testRender :: IO ()
+testRender = do
+  let svgStr = renderStateToString sampleMap sampleClaims
+  writeFile "visualiser-output.svg" svgStr
 
-mapToStr :: Map -> String
-mapToStr m = renderSvg $ mapToSvg m
+renderStateToString :: Map -> [Claim] -> String
+renderStateToString m claims = renderSvg $ mapToSvg m claims
 
-mapToSvg :: Map -> S.Svg
-mapToSvg m =
+mapToSvg :: Map -> [Claim] -> S.Svg
+mapToSvg m claims =
   S.docTypeSvg ! A.version "1.1" ! A.width "500" ! A.height "500" !
   A.viewbox (viewboxAttrs m) $ do
-    mapM_ (riverToSvg $ sites m) $ rivers m
+    mapM_ (riverToSvg claims $ sites m) $ rivers m
     mapM_ (siteToSvg $ mines m) $ sites m
 
 siteToSvg :: Set.Set SiteID -> Site -> S.Svg
@@ -30,14 +31,36 @@ siteToSvg mineIDs site =
   S.circle ! A.cx (S.toValue $ x site) ! A.cy (S.toValue $ y site) ! A.r "0.1" !
   A.fill (colourForSite mineIDs site)
 
-riverToSvg :: Set.Set Site -> River -> S.Svg
-riverToSvg allSites r =
+riverToSvg :: [Claim] -> Set.Set Site -> River -> S.Svg
+riverToSvg claims allSites r =
   S.line ! A.x1 (S.toValue (x $ endOfRiver source allSites r)) !
   A.y1 (S.toValue (y $ endOfRiver source allSites r)) !
   A.x2 (S.toValue (x $ endOfRiver target allSites r)) !
   A.y2 (S.toValue (y $ endOfRiver target allSites r)) !
-  A.strokeWidth "0.01" !
-  A.stroke "#E2E4F6"
+  A.strokeWidth (widthForRiver claims r) !
+  A.stroke (colourForRiver claims r)
+
+widthForRiver :: [Claim] -> River -> S.AttributeValue
+widthForRiver claims r =
+  if r `elem` map river claims
+    then claimedRiverWidth
+    else unclaimedRiverWidth
+
+colourForRiver :: [Claim] -> River -> S.AttributeValue
+colourForRiver claims r =
+  case maybeClaim of
+    Nothing -> unclaimedRiverColour
+    (Just claim) -> colourForPunter $ punter claim
+  where
+    maybeClaim = listToMaybe $ filter (\claim -> river claim == r) claims
+
+colourForPunter :: PunterID -> S.AttributeValue
+colourForPunter punterId =
+  case punterId of
+    0 -> firstPunterColour
+    1 -> secondPunterColour
+    2 -> thirdPunterColour
+    _ -> fourthPunterColour
 
 endOfRiver :: (River -> SiteID) -> Set.Set Site -> River -> Site
 endOfRiver f allSites r =
@@ -96,6 +119,28 @@ ys m = Set.map y $ sites m
 padding :: Double
 padding = 1.0
 
+unclaimedRiverWidth :: S.AttributeValue
+unclaimedRiverWidth = "0.03"
+
+claimedRiverWidth :: S.AttributeValue
+claimedRiverWidth = "0.04"
+
+unclaimedRiverColour :: S.AttributeValue
+unclaimedRiverColour = "#E2E4F6"
+
+firstPunterColour :: S.AttributeValue
+firstPunterColour = "#7DDF64"
+
+secondPunterColour :: S.AttributeValue
+secondPunterColour = "#8D86C9"
+
+thirdPunterColour :: S.AttributeValue
+thirdPunterColour = "#EAD637"
+
+fourthPunterColour :: S.AttributeValue
+fourthPunterColour = "#DB5461"
+
+-- SAMPLE DATA:
 sampleMap :: Map
 sampleMap =
   Map
@@ -127,3 +172,18 @@ sampleMap =
         ]
   , mines = Set.fromList [1, 5]
   }
+
+sampleClaims :: [Claim]
+sampleClaims =
+  [ Claim {punter = 0, river = River {source = 7, target = 5}}
+  , Claim {punter = 0, river = River {source = 3, target = 2}}
+  , Claim {punter = 1, river = River {source = 1, target = 2}}
+  , Claim {punter = 0, river = River {source = 5, target = 3}}
+  , Claim {punter = 1, river = River {source = 1, target = 3}}
+  , Claim {punter = 0, river = River {source = 0, target = 7}}
+  , Claim {punter = 1, river = River {source = 1, target = 7}}
+  , Claim {punter = 0, river = River {source = 4, target = 3}}
+  , Claim {punter = 1, river = River {source = 0, target = 1}}
+  , Claim {punter = 0, river = River {source = 5, target = 4}}
+  , Claim {punter = 1, river = River {source = 7, target = 6}}
+  ]
