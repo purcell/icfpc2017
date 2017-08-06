6ab5087441256ece4c8289552072fe2d5975ec22
Steve Purcell <steve@sanityinc.com>
2017-08-06 13:27:17 +1200

    More easily run "evolver" then "visualise" against example maps

--- evolver/Main.hs
+++ evolver/Main.hs
@@ -2,25 +2,28 @@
 
 module Main where
 
+import qualified Data.Set as S
 import GamePlay
 import Lib (readMap)
 import Offline (dumpState)
 import System.Environment (getArgs)
 import System.Exit
+import Types
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [mapFile, dumpFile]
+    [mapFile, dumpFile, numPlayers]
        -- Read a map in
      -> do
       Right gameMap <- readMap mapFile
+      let turns = S.size (sites gameMap) `div` read numPlayers
        -- Generate our initial state for the game
       let setup = SetupState 0 1 gameMap
       let gameState = precomputeGameState setup
        -- Iterate over some turns
-      let updatedState = iterate moveAndUpdate gameState !! 10
+      let updatedState = iterate moveAndUpdate gameState !! turns
       dumpState dumpFile updatedState
       return ()
-    _ -> die "usage: evolver MAPFILE DUMPFILE"
+    _ -> die "usage: evolver MAPFILE DUMPFILE NUMPLAYERS"
