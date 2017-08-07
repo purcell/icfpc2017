9181eb43d44b61d32d5be78bbde7437061357d2a
Steve Purcell <steve@sanityinc.com>
2017-08-07 14:59:16 +1200

    Reformulate river scoring to combine weights and a number of Int factors

--- src/Strategy.hs
+++ src/Strategy.hs
@@ -9,25 +9,43 @@ import GamePlay
 import Types
 
 data Weights = Weights
-  { wConnectedMineAgain :: Int
-  , wConnectedNewMine :: Int
+  { wConnectedNewMine :: Int
   , wEndTouchingMyRiver :: Int
   , wOnShortestMinePath :: Int
   } deriving (Show)
 
 defaultWeights :: Weights
-defaultWeights = Weights 2 30 30 10
+defaultWeights = Weights 30 30 10
+
+data RiverFactors = RiverFactors
+  { rfEndsTouchingMyRivers :: Int
+  , rfShortestMinePathCount :: Int
+  , rfConnectedNewMines :: Int
+  , rfNumPathsBetweenMines :: Int
+  , rfOptionsRemaining :: Int
+  }
+
+riverScore :: Weights -> RiverFactors -> Int
+riverScore w f =
+  product
+    [ 1 + wConnectedNewMine w * rfConnectedNewMines f
+    , wEndTouchingMyRiver w ^ rfEndsTouchingMyRivers f
+    , wOnShortestMinePath w ^
+      (2 * rfShortestMinePathCount f `div` rfNumPathsBetweenMines f)
+    ]
 
 nextMove :: Weights -> GameState -> (Move, GameState)
 nextMove ws s = (ClaimMove (myPunterID s) (bestUnclaimedRiver ws s), s)
 
 bestUnclaimedRiver :: Weights -> GameState -> River
 bestUnclaimedRiver w s =
-  case listToMaybe $ sortBy (flip $ comparing riverScore) unclaimedRivers of
+  case listToMaybe $
+       sortBy (flip $ comparing (riverScore w . riverFactors)) unclaimedRivers of
     Just r -> r
     _ -> error "how could there not be an unclaimed river?"
   where
     theMap = GamePlay.map (initialState s)
+    -- TODO: include rivers where options are available
     unclaimedRivers = S.toList $ allRivers \\ claimedRivers
     allRivers = rivers theMap
     claimedRivers = S.map claimRiver (claims s)
@@ -42,27 +60,15 @@ bestUnclaimedRiver w s =
         (S.toList (claims s))
     mineSites :: Set SiteID
     mineSites = mines theMap
-    riverScore :: River -> Int
-    riverScore r =
-      (1 + sum (connectedMineScore <$> connectedMineSites r)) *
-      (wEndTouchingMyRiver w ^ endsTouchingMyRivers r) *
-      (1 +
-       (wOnShortestMinePath w * shortestMinePathCount r) `div`
-       numPathsBetweenMines)
-    endsTouchingMyRivers :: River -> Int
-    endsTouchingMyRivers r = S.size (S.filter (connectedTo r) mySites)
-    numPathsBetweenMines = length $ shortestMinePaths s
-    shortestMinePathCount :: River -> Int
-    shortestMinePathCount r = sum [1 | p <- shortestMinePaths s, r `S.member` p]
+    riverFactors r =
+      RiverFactors
+      { rfEndsTouchingMyRivers = S.size (S.filter (connectedTo r) mySites)
+      , rfShortestMinePathCount =
+          sum [1 | p <- shortestMinePaths s, r `S.member` p]
+      , rfConnectedNewMines =
+          sum [1 | m <- connectedMineSites r, m `S.notMember` mySites]
+      , rfNumPathsBetweenMines = length $ shortestMinePaths s
+      , rfOptionsRemaining = optionsRemaining s (myPunterID s)
+      }
     connectedMineSites :: River -> [SiteID]
     connectedMineSites r = S.toList $ S.filter (connectedTo r) mineSites
-    connectedMineScore :: SiteID -> Int
-    connectedMineScore m =
-      if S.member m mySites
-        then wConnectedMineAgain w
-        else wConnectedNewMine w
-
-data RiverFactors = RiverFactors
-  { rfEndsTouchingMyRivers :: Int
-  , rfShortestMinePathCount :: Int
-  }
